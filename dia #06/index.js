b8643330a7e3205bcbff1d15c922dff55d51acc4
Gustavo Prado <144186260+gustavoprado0@users.noreply.github.com>
2023-11-19 17:51:28 -0300

    index.js

--- dia #06/index.js
+++ dia #06/index.js
@@ -2,6 +2,6 @@ let tabuada = Number(prompt("Qual número deseja ?"))
 
 
 for (let contador = 0; contador <= 10; contador++){
-  console.log(tabuada + " x " + contador  +  " = " +  tabuada / contador )
+  console.log(tabuada + " x " + contador  +  " = " +  tabuada * contador )
 
 }
