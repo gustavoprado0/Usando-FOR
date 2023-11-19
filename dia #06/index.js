a044c7dc2070178ae7227d125eca6311b9fc35f6
Gustavo Prado <144186260+gustavoprado0@users.noreply.github.com>
2023-11-19 17:51:03 -0300

    Add files via upload
    Initial commit

---
+++ dia #06/index.js
@@ -0,0 +1,7 @@
+let tabuada = Number(prompt("Qual número deseja ?"))
+
+
+for (let contador = 0; contador <= 10; contador++){
+  console.log(tabuada + " x " + contador  +  " = " +  tabuada / contador )
+
+}
